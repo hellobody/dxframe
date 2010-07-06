c174e1229930d2a04c90bbd9fcadaf6dd04edba4
hellobody0 <hellobody0@d3722006-5678-7fee-70ad-d29c3430689b>
2010-07-06 19:35:19 +0000

    two triangles

--- trunk/dxframe/dxframe/WinMain.cpp
+++ trunk/dxframe/dxframe/WinMain.cpp
@@ -5,15 +5,34 @@ BOOL bActive;
 LPCWSTR APPNAME = L"dxframe";
 LPCWSTR APPTITLE = L"dxframe";
 
+//interfaces
+LPDIRECT3D8 p_d3d = NULL;
+LPDIRECT3DDEVICE8 p_d3d_Device = NULL;
+LPDIRECT3DVERTEXBUFFER8 p_VertexBuffer = NULL;
+
+CUSTOMVERTEX g_Vertices[] = {
+	{  60.0f,  60.0f, 0.5f, 1.0f, 0xffff0000, },
+	{ 200.0f,  60.0f, 0.5f, 1.0f, 0xff00ff00, },
+	{  60.0f, 200.0f, 0.5f, 1.0f, 0xff0000ff, },
+
+	{  80.0f, 220.0f, 0.5f, 1.0f, 0xff0000ff, },
+	{ 220.0f,  80.0f, 0.5f, 1.0f, 0xff00ff00, },
+	{ 220.0f, 220.0f, 0.5f, 1.0f, 0xffffff00, },
+};
+
 LRESULT CALLBACK WindowProc (HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
 	switch (message) {
 		case WM_ACTIVATE:
 			bActive = LOWORD (wParam);
 			break;
 		case WM_DESTROY:
+			RELEASE (p_VertexBuffer);
+			RELEASE (p_d3d_Device);
+			RELEASE (p_d3d);
 			PostQuitMessage (0);
 			break;
 		case WM_SETCURSOR:
+			SetCursor (NULL);
 			break;
 	} return DefWindowProc (hWnd, message, wParam, lParam);
 }
@@ -47,11 +66,40 @@ bool AppInit (HINSTANCE hThisInst, int nCmdShow) {
 	ShowWindow (hWnd, nCmdShow);
 	UpdateWindow (hWnd);
 
+	p_d3d = Direct3DCreate8 (D3D_SDK_VERSION);
+
+	D3DDISPLAYMODE d3ddm;
+	p_d3d->GetAdapterDisplayMode (D3DADAPTER_DEFAULT, &d3ddm);
+
+	D3DPRESENT_PARAMETERS d3dpp;
+	ZeroMemory (&d3dpp, sizeof (d3dpp));
+	d3dpp.Windowed = true;
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	d3dpp.BackBufferFormat = d3ddm.Format;
+
+	p_d3d->CreateDevice (D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &p_d3d_Device);
+
+	p_d3d_Device->CreateVertexBuffer (6*sizeof (CUSTOMVERTEX), 0, D3DFVF_CUSTOMVERTEX, D3DPOOL_DEFAULT, &p_VertexBuffer);
+
+	VOID * pVertices;
+	p_VertexBuffer->Lock (0, sizeof (g_Vertices), (BYTE**)&pVertices, 0);
+	memcpy (pVertices, g_Vertices, sizeof (g_Vertices));
+	p_VertexBuffer->Unlock ();
+
 	return true;
 }
 
 void Render () {
+	
+	p_d3d_Device->Clear (0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB (0, 0, 0), 1.f, 0);
+	p_d3d_Device->BeginScene ();
 
+	p_d3d_Device->SetVertexShader (D3DFVF_CUSTOMVERTEX);
+	p_d3d_Device->SetStreamSource (0, p_VertexBuffer, sizeof (CUSTOMVERTEX));
+	p_d3d_Device->DrawPrimitive (D3DPT_TRIANGLELIST, 0, 2);
+
+	p_d3d_Device->EndScene ();
+	p_d3d_Device->Present (NULL, NULL, NULL, NULL);
 }
 
 int APIENTRY WinMain (HINSTANCE hThisInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow) {
