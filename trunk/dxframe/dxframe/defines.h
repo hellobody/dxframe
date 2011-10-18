032cafcd4c1993dfd73c5345eeac9bc23555d169
hellobody0@gmail.com <hellobody0@gmail.com@d3722006-5678-7fee-70ad-d29c3430689b>
2011-10-18 15:47:40 +0000

    возникли проблемы с directx input в часности падает метод Acquire () сделаю попытку вернуться к directx 8 версии (т.к. баг появился похоже после перехода на 9-ую версию) на шейдеры пока забью, хочеться делать игру уже

--- trunk/dxframe/dxframe/defines.h
+++ trunk/dxframe/dxframe/defines.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#define DIRECTINPUT_VERSION 0x0800
+
 //#include "vld.h" //comment me
 #include <time.h>
 #include <d3d9.h>
@@ -22,8 +24,9 @@
 
 #pragma comment (lib, "d3d9.lib")
 #pragma comment (lib, "d3dx9.lib")
-#pragma comment (lib, "dxguid.lib")
+#pragma comment (lib, "dinput.lib")
 #pragma comment (lib, "dinput8.lib")
+#pragma comment (lib, "dxguid.lib")
 
 #ifdef DXFRAME_EXPORTS
 #define DXFRAME_API __declspec(dllexport)
--- trunk/dxframe/dxframe/dxInput.h
+++ trunk/dxframe/dxframe/dxInput.h
@@ -7,6 +7,15 @@ class DXFRAME_API dxInput {
 
 protected:
 
+	LPDIRECTINPUT8 din;				// the pointer to our DirectInput interface
+	LPDIRECTINPUTDEVICE8 dinkeybd;    // the pointer to the keyboard device
+	LPDIRECTINPUTDEVICE8 dinmouse;    // the pointer to the mouse device
+
+	DIMOUSESTATE lstMousestate;    // the storage for the last mouse-information
+	DIMOUSESTATE mousestate;    // the storage for the mouse-information
+	BYTE lstKeystate [256];    // the storage for the last key-information
+	BYTE keystate [256];    // the storage for the key-information
+
 	LPPOINT CursorPosition;
 
 	HWND hWnd;
